0f77ab21edaac14bae709fe1d500b9296cbb8755
uvsq21807869 <21807869@ens.uvsq.fr>
2020-03-27 19:01:03 +0100

    la definition de l amethode qui permet d'appliquer une operation sur les operandes

--- src/main/java/uvsq/fr/pgpl_4_2/MoteurRPN.java
+++ src/main/java/uvsq/fr/pgpl_4_2/MoteurRPN.java
@@ -2,7 +2,6 @@ package uvsq.fr.pgpl_4_2;
 
 import java.util.Stack;
 
-
 public class MoteurRPN extends Interpreteur {
 
 
@@ -18,4 +17,25 @@ public class MoteurRPN extends Interpreteur {
 		temp.addAll(operandes);
 		operandesPrecedent.add(temp);
 	}
+	// appliquer une operation sur les operandes
+		public void appliquerOperation(Operations operation) throws ArithmeticException {
+			if (operandes.size() >= 2) {
+
+				double val1 = operandes.pop();
+
+				double val2 = operandes.pop();
+
+				operandes.add(operation.eval(val1, val2));
+				
+				
+				Stack<Double> temp=new Stack<Double>();
+				temp.addAll(operandes);
+				operandesPrecedent.add(temp);		
+				
+			}
+
+			else
+				throw new ArithmeticException("vos operande (pas asez d'operandes pour effectuer l'operation)");
+		}
+
 }
